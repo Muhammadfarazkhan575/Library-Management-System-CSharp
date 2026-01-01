5f1af53abf35b35e8a1123d8399cbd05a7f60ca5
Muhammadfarazkhan575 <farazkhan03367071@gmail.com>
2026-01-01 12:50:37 +0500

    Add Book.cs

---
+++ book.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagementSystem
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+    }
+}
